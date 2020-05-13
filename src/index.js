954be5e55d251743ac1e23907fc1097fc7891439
TheBusyBiscuit <TheBusyBiscuit@users.noreply.github.com>
2020-05-13 22:21:53 +0200

    Fixed a small mistake

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ const webhook = require('../src/discord.js');
 async function run() {
 	const repository = github.context.repo;
 	const payload = github.context.payload;
-    const commits = payload.commits;
-    const size = payload.size;
-    const diff = `https://github.com/${repository}/compare/${payload.before}...${payload.after}`;
+  	const commits = payload.commits;
+    	const size = payload.size;
+    	const diff = `https://github.com/${repository}/compare/${payload.before}...${payload.after}`;
 	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1];
 
 	console.log(`Received ${commits.length}/${size} commits...`);
 
-    const id = core.getInput("id");
-    const id = core.getInput("token");
+    	const id = core.getInput("id");
+    	const token = core.getInput("token");
 
 	analysis.start().then((report) => {
         webhook.send(id, token, repository, branch, diff, commits, size, report).catch(err => core.setFailed(err.message));
