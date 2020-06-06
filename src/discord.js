00a8c15f7be5b2cc4ff2752cb70ba9e739d43676
Thomas Bnt <thomasbnt@protonmail.com>
2020-06-06 20:58:54 +0200

    Remove terminaison and var to const

--- src/discord.js
+++ src/discord.js
@@ -1,56 +1,52 @@
-const discord = require('discord.js');
-const MAX_MESSAGE_LENGTH = 40;
+const discord = require('discord.js')
+const MAX_MESSAGE_LENGTH = 40
 
 module.exports.send = (id, token, repo, branch, url, commits, size, report) => new Promise((resolve, reject) => {
-    var client;
-    console.log("Preparing Webhook...");
+    const client
+    console.log("Preparing Webhook...")
     try {
-        client = new discord.WebhookClient(id, token);
+        client = new discord.WebhookClient(id, token)
     }
     catch (error) {
-        reject(error.message);
-        return;
+        reject(error.message)
+        return
     }
 
     client.send(createEmbed(repo, branch, url, commits, size, report)).then(() => {
-        console.log("Successfully sent the message!");
-        resolve();
-    }, reject);
-});
+        console.log("Successfully sent the message!")
+        resolve()
+    }, reject)
+})
 
 function createEmbed(repo, branch, url, commits, size, report) {
-    console.log("Constructing Embed...");
-    var latest = commits[0];
+    console.log("Constructing Embed...")
+    const latest = commits[0]
 
-    var embed = new discord.RichEmbed()
+    const embed = new discord.RichEmbed()
                 .setColor(getEmbedColor(report))
                 //.setTitle(size + (size == 1 ? " Commit was " : " Commits were ") + "added to " + repo + " (" + branch + ")")
                 .setTitle(size + (size == 1 ? " Commit was " : " Commits were ") + "added to " + banch)
                 .setDescription(getChangeLog(commits, size))
-                .setTimestamp(Date.parse(latest.timestamp));
+                .setTimestamp(Date.parse(latest.timestamp))
 
-    if (report.tests.length > 0) {
-        appendTestResults(embed, report);
-    }
-
-    return embed;
+    if (report.tests.length > 0) { appendTestResults(embed, report) }
+    return embed
 }
 
 function getChangeLog(commits, size) {
-    var changelog = "";
-
-    for (var i in commits) {
+    const changelog = ""
+    for (const i in commits) {
         if (i > 3) {
-            changelog += `+ ${size - i} more...\n`;
-            break;
+            changelog += `+ ${size - i} more...\n`
+            break
         }
 
-        var commit = commits[i];
-        var sha = commit.id.substring(0, 6);
-        var message = commit.message.length > MAX_MESSAGE_LENGTH ? (commit.message.substring(0, MAX_MESSAGE_LENGTH) + "..."): commit.message;
-        changelog += `[\`${sha}\`](${commit.url}) — ${message} \n`;
+        const commit = commits[i];
+        const sha = commit.id.substring(0, 6)
+        const message = commit.message.length > MAX_MESSAGE_LENGTH ? (commit.message.substring(0, MAX_MESSAGE_LENGTH) + "..."): commit.message
+        changelog += `[\`${sha}\`](${commit.url}) — ${message} \n`
     }
 
-    return changelog;
+    return changelog
 }
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-const analysis = require('../src/analysis.js');
-const webhook = require('../src/discord.js');
+const analysis = require('../src/analysis.js')
+const webhook = require('../src/discord.js')
 
 async function run() {
-	const payload = github.context.payload;
-	const repository = payload.repository.full_name;
-	const commits = payload.commits;
-	const size = commits.length;
-	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1];
+	const payload = github.context.payload
+	const repository = payload.repository.full_name
+	const commits = payload.commits
+	const size = commits.length
+	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1]
 
-	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`);
+	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`)
 
-	console.log(`Received ${commits.length}/${size} commits...`);
+	console.log(`Received ${commits.length}/${size} commits...`)
 
-    	const id = core.getInput("id");
-    	const token = core.getInput("token");
+    	const id = core.getInput("id")
+    	const token = core.getInput("token")
 
 	//analysis.start(isSkipped(payload.head_commit)).then((report) => {
-        webhook.send(id, token, repository, branch, payload.compare, commits, size, report).catch(err => core.setFailed(err.message));
-    //}, err => core.setFailed(err));
+        webhook.send(id, token, repository, branch, payload.compare, commits, size, report).catch(err => core.setFailed(err.message))
+    //}, err => core.setFailed(err))
 }
 
 try {
-	run();
-} catch (error) {
-    core.setFailed(error.message);
-}
+    run()
+} catch (error) { core.setFailed(error.message) }
 
 function isSkipped(commit) {
-	return commit.message.toLowerCase().includes("[skip]");
+	return commit.message.toLowerCase().includes("[skip]")
 }
