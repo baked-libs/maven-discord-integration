32c7f50ee206f32c72abe59165394a2d97526cd8
Thomas Bnt <thomasbnt@protonmail.com>
2020-06-06 22:01:08 +0200

    Edit var to const and put id/token on README on Array

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const analysis = require('../src/analysis.js')
 const webhook = require('../src/discord.js')
 
 async function run() {
-	var payload = github.context.payload
-	var repository = payload.repository.full_name
-	var commits = payload.commits
-	var size = commits.length
-	var branch = payload.ref.split('/')[payload.ref.split('/').length - 1]
+	const payload = github.context.payload
+	const repository = payload.repository.full_name
+	const commits = payload.commits
+	const size = commits.length
+	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1]
 
 	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`)
 
 	console.log(`Received ${commits.length}/${size} commits...`)
 
-    	var id = core.getInput("id")
-    	var token = core.getInput("token")
+    	const id = core.getInput("id")
+    	const token = core.getInput("token")
 
-	analysis.start(isSkipped(payload.head_commit)).then((report) => {
+	payload.head_commit.then((report) => {
         webhook.send(id, token, repository, branch, payload.compare, commits, size, report).catch(err => core.setFailed(err.message))
     }, err => core.setFailed(err))
 }
