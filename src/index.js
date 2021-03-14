fb8a1ad6c10c870b2c50fff507bb6d117834494c
TheBusyBiscuit <TheBusyBiscuit@users.noreply.github.com>
2021-03-14 13:32:16 +0100

    Fixed failure when pushing tags.

--- src/index.js
+++ src/index.js
@@ -12,11 +12,16 @@ async function run() {
 	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1];
 
 	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`);
-
 	console.log(`Received ${commits.length}/${size} commits...`);
 
-    	const id = core.getInput("id");
-    	const token = core.getInput("token");
+	if (commits.length === 0) {
+        // This was likely a "--tags" push.
+        console.log(`Aborting analysis, found no commits.`);
+		return;
+	}
+
+    const id = core.getInput("id");
+    const token = core.getInput("token");
 
 	analysis.start(isSkipped(payload.head_commit)).then((report) => {
         webhook.send(id, token, repository, branch, payload.compare, commits, size, report).catch(err => core.setFailed(err.message));
