9c990d0a27bd9bf96e1676b807d3b10af3aaefeb
Thomas Bnt <thomasbnt@protonmail.com>
2022-04-07 19:43:37 +0200

    Cleanup and ?

--- src/discord.js
+++ src/discord.js
@@ -1,52 +1,56 @@
 const discord = require('discord.js')
 const MAX_MESSAGE_LENGTH = 40
 
-module.exports.send = (id, token, repo, branch, url, commits, size) => new Promise((resolve, reject) => {
+module.exports.send = (id, token, repo, branch, url, commits, size) =>
+  new Promise((resolve, reject) => {
     var client
-    console.log("Preparing Webhook...")
+    console.log('Preparing Webhook...')
     try {
-        client = new discord.WebhookClient({id: id, token: token})
-    }
-    catch (error) {
-        console.log("Error creating Webhook")
-        reject(error.message)
-        return
+      client = new discord.WebhookClient({ id: id, token: token })
+    } catch (error) {
+      console.log('Error creating Webhook')
+      reject(error.message)
+      return
     }
 
     client.send(createEmbed(repo, branch, url, commits, size)).then(() => {
-        console.log("Successfully sent the message!")
-        resolve()
+      console.log('Successfully sent the message!')
+      resolve()
     }, reject)
-})
+  })
 
 function createEmbed(repo, branch, url, commits, size) {
-    console.log("Constructing Embed...")
-    const latest = commits[0]
+  console.log('Constructing Embed...')
+  var latest = commits[0]
 
-    return new discord.MessageEmbed()
-        .setColor(0x00BB22)
-        //.setTitle(size + (size == 1 ? " Commit was " : " Commits were ") + "added to " + repo + " (" + branch + ")")
-        .setTitle(`${size} ${size === 1 ? "commit was " : "commits were"}  added to ${branch}`)
-        .setDescription(getChangeLog(commits, size))
-        .setTimestamp(Date.parse(latest.timestamp))
-        .setFooter({
-            text: `⚡ Edited by @${commits[0].author.username}`
-        })
+  var embed = new discord.MessageEmbed()
+    .setColor(0x00bb22)
+    //.setTitle(size + (size == 1 ? " Commit was " : " Commits were ") + "added to " + repo + " (" + branch + ")")
+    .setTitle(`${size} ${size === 1 ? 'commit was ' : 'commits were'}  added to ${branch}`)
+    .setDescription(getChangeLog(commits, size))
+    .setTimestamp(Date.parse(latest.timestamp))
+    .setFooter({
+      text: `⚡ Edited by @${commits[0].author.username}`,
+    })
+  return embed
 }
 
 function getChangeLog(commits, size) {
-    var changelog = ""
-    for (var i in commits) {
-        if (i > 7) {
-            changelog += `+ ${size - i} more...\n`
-            break
-        }
-
-        var commit = commits[i];
-        var sha = commit.id.substring(0, 6)
-        var message = commit.message.length > MAX_MESSAGE_LENGTH ? (commit.message.substring(0, MAX_MESSAGE_LENGTH) + "..."): commit.message
-        changelog += `[\`${sha}\`](${commit.url}) — ${message}\n`
+  var changelog = ''
+  for (var i in commits) {
+    if (i > 7) {
+      changelog += `+ ${size - i} more...\n`
+      break
     }
 
-    return changelog
+    var commit = commits[i]
+    var sha = commit.id.substring(0, 6)
+    var message =
+      commit.message.length > MAX_MESSAGE_LENGTH
+        ? commit.message.substring(0, MAX_MESSAGE_LENGTH) + '...'
+        : commit.message
+    changelog += `[\`${sha}\`](${commit.url}) — ${message}\n`
+  }
+
+  return changelog
 }
--- src/index.js
+++ src/index.js
@@ -4,23 +4,26 @@ const github = require('@actions/github')
 const webhook = require('../src/discord.js')
 
 async function run() {
-	const payload = github.context.payload
-	const repository = payload.repository.full_name
-	const commits = payload.commits
-	const size = commits.length
-	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1]
+  const payload = github.context.payload
+  const repository = payload.repository.full_name
+  const commits = payload.commits
+  const size = commits.length
+  const branch = payload.ref.split('/')[payload.ref.split('/').length - 1]
 
-	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`)
+  console.log(`Received payload ${JSON.stringify(payload, null, 2)}`)
 
-	console.log(`Received ${commits.length}/${size} commits...`)
+  console.log(`Received ${commits.length}/${size} commits...`)
 
-    	const id = core.getInput("id")
-    	const token = core.getInput("token")
+  const id = core.getInput('id')
+  const token = core.getInput('token')
 
-    webhook.send(id, token, repository, branch, payload.compare, commits, size).catch(err => core.setFailed(err.message));
-    
+  webhook
+    .send(id, token, repository, branch, payload.compare, commits, size)
+    .catch((err) => core.setFailed(err.message))
 }
 
 try {
-    run()
-} catch (error) { core.setFailed(error.message) }
+  run()
+} catch (error) {
+  core.setFailed(error.message)
+}
